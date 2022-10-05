9011ec6d033d894f8d25138c95076382ce8335ab
afisher000 <43121839+afisher000@users.noreply.github.com>
2022-10-04 18:05:07 -0700

    updates

--- Datasets/Housing Prices/model.py
+++ Datasets/Housing Prices/model.py
@@ -8,15 +8,22 @@ Created on Sun Oct  2 16:09:17 2022
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import sklearn.linear_model
+from sklearn.linear_model import LinearRegression, Ridge, RidgeCV
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.inspection import PartialDependenceDisplay
-from time import time
+from sklearn.model_selection import cross_val_score
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
 
+from time import time
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+
+# Load data
 df = pd.read_csv('train.csv')
 
+# Choose features for X and y
 features = [
-    #'LotArea',
+    'LotArea',
     #'LandContour',
     #'Neighborhood',
     'OverallQual',
@@ -25,20 +32,32 @@ features = [
 
 target = 'SalePrice'
 
+def print_model_results(model, X, y, model_type):
+    ts = time()
+
+    squared_scores = -1*cross_val_score(
+        model, X, y, scoring='neg_mean_squared_error', cv=10
+        )
+    scores = np.sqrt(squared_scores)
+
+    print(model_type)
+    print(f'\tError = {scores.mean():.0f} ({scores.std():.0f})')
+    print(f'\tTime = {time()-ts:.2f}s')
+    return
+
+
 y = df[target].values
 X = df[features].values
 
+pipe = Pipeline([('Scale',StandardScaler())])
 
-##### How to reduce dimensions
 
+# Simple linear regression
+linreg = LinearRegression()
+print_model_results(linreg, X, y, 'Simple Linear Regression')
 
-## Partial Dependence Plots
-st = time()
-model = RandomForestClassifier(max_depth=2).fit(X,y)
-print('%-20s: %.2fs' % ('Time to train model',time()-st) )
-
-features = [0, 1]
-PartialDependenceDisplay.from_estimator(model, X, features, target=0)
-
+# Ridge Regression
+ridge = Ridge(alpha=500)
+print_model_results(ridge, X, y, 'Ridge Regression')
 
 
